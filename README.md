11e75d55edaa4244e2edf9cf31895291e1e6519b
hirohata <43802663+hitohata@users.noreply.github.com>
2023-10-30 08:45:38 -0700

    18 create GitHub actions for deploying the open api document (#31)
    * feat: add docker compose
    
    * feat: add an index.hml to redirect to the project document
    
    * feat: change a build step
    
    * feat: deleted no needed step
    
    * feat: updated a .gitignore
    
    * feat: added a link to the documents on the README.md
    
    * fix: base url

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # documents
+
+## Documents
+
+[Project Document](https://genesis-tech-tribe.github.io/nishiki-documents/project-document/index.html)
+[Web API Document](https://genesis-tech-tribe.github.io/nishiki-documents/web-api/index.html)
--- project-document/docusaurus.config.js
+++ project-document/docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://genesis-tech-tribe.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/nishiki-documents',
+  baseUrl: '/nishiki-documents/project-document',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
